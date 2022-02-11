41bf369d83e5fed510830841813e239907855f7c
Your Name <you@example.com>
2022-02-11 14:44:21 +0200

    created and completed

---
+++ src/main/java/com/example/springapp/dto/DepartmentDto.java
@@ -0,0 +1,14 @@
+package com.example.springapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DepartmentDto {
+    private String country;
+    private String city;
+    private String address;
+}
---
+++ src/main/java/com/example/springapp/dto/UserDto.java
@@ -0,0 +1,15 @@
+package com.example.springapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    private String firstName;
+    private String lastName;
+    private String country;
+    private String email;
+}
