d402576bdf42fc80d77d19dfa1183ce316ac844a
Your Name <you@example.com>
2022-02-12 21:11:54 +0200

    update

--- src/main/java/com/example/springapp/dto/ProductDto.java
+++ src/main/java/com/example/springapp/dto/ProductDto.java
@@ -1,5 +1,6 @@
 package com.example.springapp.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,8 +8,12 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Сущность продукта")
 public class ProductDto {
+    @Schema(description = "Название продукта", example = "Продукт")
     private String name;
+    @Schema(description = "Количество продукта", example = "100")
     private int quantity;
+    @Schema(description = "Цена продукта", example = "100$")
     private double price;
 }
