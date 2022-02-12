cbef410ffacb9ed01ee5402de435131ee8d849cd
Your Name <you@example.com>
2022-02-12 20:37:49 +0200

    update

--- src/main/java/com/example/springapp/controller/ProductController.java
+++ src/main/java/com/example/springapp/controller/ProductController.java
@@ -1,51 +1,81 @@
 package com.example.springapp.controller;
 
+import com.example.springapp.domain.Department;
 import com.example.springapp.domain.Product;
+import com.example.springapp.dto.DepartmentDto;
+import com.example.springapp.dto.ProductDto;
+import com.example.springapp.mapper.DepartmentMapper;
+import com.example.springapp.mapper.ProductMapper;
 import com.example.springapp.service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Product Controller", description = "The Product API")
+@RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
 @RestController
 public class ProductController {
-    @Autowired
-    private ProductService service;
 
+    private final ProductService service;
+
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
+
+    @Operation(summary = "Сохраняет продукт ")
     @PostMapping("/addProduct")
-    public Product addProduct(@RequestBody Product product) {
-        return service.saveProduct(product);
+    public ResponseEntity<ProductDto> addProduct(@RequestBody ProductDto productDto) {
+        Product product = ProductMapper.INSTANCE.productDtoToProduct(productDto);
+        service.saveProduct(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productDto);
     }
 
+    @Operation(summary = "Сохраняет продукт")
     @PostMapping("/addProducts")
-    public List<Product> addProducts(@RequestBody List<Product> products) {
-        return service.saveProducts(products);
+    public ResponseEntity<List<ProductDto>> addProducts(@RequestBody List<Product> products) {
+        List<ProductDto> productDtos = ProductMapper.INSTANCE.productToDtos(service.saveProducts(products));
+        return ResponseEntity.status(HttpStatus.OK).body(productDtos);
     }
 
+    @Operation(summary = "Возвращает продукт")
     @GetMapping("/products")
-    public List<Product> findAlProducts() {
-        return service.getProducts();
+    public ResponseEntity<List<ProductDto>> findAlProducts() {
+        List<ProductDto> productDtos = ProductMapper.INSTANCE.productToDtos(service.getProducts());
+        return ResponseEntity.status(HttpStatus.OK).body(productDtos);
     }
 
+    @Operation(summary = "Возвращает продукт по id")
     @GetMapping("/productById/{id}")
-    public Product findProductById(@PathVariable int id) {
-        return service.getProductById(id);
+    public ResponseEntity<ProductDto> findProductById(@PathVariable Integer id) {
+        ProductDto productDto = ProductMapper.INSTANCE.productToDto(service.getProductById(id));
+        return ResponseEntity.status(HttpStatus.OK).body(productDto);
     }
 
+    @Operation(summary = "Возвращает продукт по названию")
     @GetMapping("/product/{name}")
-    public Product findProductByName(@PathVariable String name) {
-        return service.getProductByName(name);
+    public ResponseEntity<ProductDto> findProductByName(@PathVariable String name) {
+        ProductDto productDto = ProductMapper.INSTANCE.productToDto(service.getProductByName(name));
+        return ResponseEntity.status(HttpStatus.OK).body(productDto);
     }
 
+    @Operation(summary = "Обновляет продукт")
     @PutMapping("/updateProduct")
-    public Product updateProduct(@RequestBody Product product) {
-        return service.updateProduct(product);
+    public ResponseEntity<ProductDto> updateProduct(@RequestBody Product product) {
+        ProductDto productDto = ProductMapper.INSTANCE.productToDto(service.updateProduct(product));
+        return ResponseEntity.status(HttpStatus.OK).body(productDto);
     }
 
-    @DeleteMapping("/deleteProduct/{id}")
-    public String  deleteProduct(@PathVariable int id) {
-        return service.deleteProduct(id);
-    }
+    @Operation(summary = "Удаляет продукт по id")
+    @DeleteMapping("/smartphones/{id}")
+    public ResponseEntity<String>  deleteProduct(@PathVariable Integer id) {
+       service.deleteProduct(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Product with id : " + " deleted!");
 
+    }
 
 }
